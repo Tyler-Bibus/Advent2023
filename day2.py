3acb5ee83adcdd051b6a66f1b8d927bf1d3b2dda
Tyler-Bibus <tylerbibus@hotmail.com>
2023-12-05 10:55:08 -0600

    added python file and started work

---
+++ day2.py
@@ -0,0 +1,32 @@
+import time
+import re
+
+def real(arg, num):
+	bool possible = False;
+	#print("real called")
+	if arg
+	return num
+
+
+
+
+#opens file input
+file1 = open('input.txt', 'r')
+
+#gets line one at a time
+total = 0;
+count = 1
+
+while True:
+	line = file1.readline()
+	if not line:
+		break
+	print("{}".format(line))
+	total += real(line, count)
+	count += 1
+
+print("{}".format(total))
+
+print("end of program")
+#pauses for 10s to see ouput
+time.sleep(10)
