faac1516eeed2c96ebfa9d437833c6a23448cb59
Tyler-Bibus <tylerbibus@hotmail.com>
2023-12-07 00:27:02 -0600

    Working Day 2 Part one Solution

--- day2.py
+++ day2.py
@@ -1,15 +1,48 @@
 import time
 import re
 
-def real(arg, num):
-	possible = False
+def real(arg):
+	possible = True
+	checks = 0
+
 	#print("real called")
-	if arg:
-		possible = True
-	return num
+
+	#stores colors in a list
+	colors = []
+
+	#finds all the numbers
+	nums = re.findall(r'\d+', arg)
+	numlist = list(map(int, nums))
+
+	#finds order of colors
+	#colors = re.findall(r'[rgb]', arg)
+	colors = re.findall(r'red|green|blue', arg)
+	print(colors)
+	print("numbers in list ar: " + str(numlist))
+
+	#finds if possible
+	checks = len(colors)
+	print("Length of color list: " + str(checks))
+	i = 0
+	print("checking...")
+	for i in range(checks):
+		if colors[i] == 'red' and numlist[i+1] > 12:
+			possible = False
+			print("not possible")
+			return 0
+		if colors[i] == 'green' and numlist[i+1] > 13:
+			possible = False
+			print("not possible")
+			return 0
+		if colors[i] == 'blue' and numlist[i+1] > 14:
+			possible = False
+			print("not possible")
+			return 0
+
+	return numlist[0]
 
 #opens file input
-file1 = open('input.txt', 'r')
+file1 = open('inputFin.txt', 'r')
 
 #gets line one at a time
 total = 0
@@ -20,11 +53,11 @@ while True:
 	if not line:
 		break
 	print("{}".format(line))
-	total += real(line, count)
+	total += real(line)
 	count += 1
 
 print("{}".format(count))
-
+print(total)
 print("end of program")
 #pauses for 10s to see ouput
-time.sleep(10)
+time.sleep(2)
